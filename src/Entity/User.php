ea3534ebc52aa4fc6fb7f9ccd143161456301e23
yverrom <yverrom@ndlp.fr>
2020-01-10 11:29:39 +0100

    Branch security

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -70,8 +70,6 @@ class User implements UserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-        $roles[] = 'ROLE_ADMIN';
-        $roles[] = 'ROLE_SUPER-ADMIN';
 
         return array_unique($roles);
     }
